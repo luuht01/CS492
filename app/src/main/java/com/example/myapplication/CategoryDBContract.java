0f10e174e92331beb97439510efd95744fe1f3f8
Chris Heffley <heffcr01@pfw.edu>
2019-03-25 16:46:03 -0400

    Initial DB Code, Search and create activity fleshed out. Main activity wired up for testing DB

--- app/src/main/java/com/example/myapplication/CategoryDBContract.java
+++ app/src/main/java/com/example/myapplication/CategoryDBContract.java
@@ -2,20 +2,20 @@ package com.example.myapplication;
 
 import android.provider.BaseColumns;
 
-public class CategoryDBContract {
+    class CategoryDBContract {
     private CategoryDBContract(){}
 
-public static class CategoryEntry implements BaseColumns {
-    public static final String TableName = "ItemCategories";
-    public static final String ColCategoryName = "CategoryName";
-    public static final String ColCategoryKey = "Id";
+    static class CategoryEntry implements BaseColumns {
+    static final String TableName = "ItemCategories";
+    static final String ColCategoryName = "CategoryName";
+    static final String ColCategoryKey = "Id";
 
 }
 
-    public static final String CreateTable =
+    static final String CreateTable =
             "Create Table " +  CategoryEntry.TableName + " (" +
                     CategoryEntry.ColCategoryKey + " integer primary key autoincrement, " +
                     CategoryEntry.ColCategoryName + " text)";
 
-    public static final String DropTable = "drop table if exists " + CategoryEntry.TableName;
+    static final String DropTable = "drop table if exists " + CategoryEntry.TableName;
 }
--- app/src/main/java/com/example/myapplication/Item.java
+++ app/src/main/java/com/example/myapplication/Item.java
@@ -7,14 +7,14 @@ import java.util.ArrayList;
 
 public class Item {
     protected long id;
-    protected String productNumber;
-    protected String description;
-    protected String websiteUrl;
-    protected double price;
-    protected User updateBy;
-    protected  Category category;
-    protected ArrayList<String> keywords;
-    protected ZonedDateTime updateDate;
+    private String productNumber;
+    private String description;
+    private String websiteUrl;
+    private double price;
+    private User updateBy;
+    private Category category;
+    private ArrayList<String> keywords;
+    private ZonedDateTime updateDate;
 
 
     //private ShopcastDBHelper DBhelper;
@@ -40,65 +40,68 @@ public class Item {
 
     }
     //Setters
-    public void setProductNumber(String _productNumber) {
+    void setProductNumber(String _productNumber) {
         this.productNumber = _productNumber;
     }
 
-    public void setDescription(String _description) {
+    void setDescription(String _description) {
         this.description = _description;
     }
 
-    public void setWebsiteUrl(String _websiteUrl) {
+    void setWebsiteUrl(String _websiteUrl) {
         this.websiteUrl = _websiteUrl;
     }
 
-    public void setPrice(double _price) {
+    void setPrice(double _price) {
         this.price = _price;
     }
 
-    public void setCategory(Category _category){this.category=_category; }
-public void setUpdateUser(User _user){this.updateBy=_user;}
-    public void addKeyword(String _keyword) {
+    void setCategory(Category _category){this.category=_category; }
+
+    void setUpdateUser(User _user){this.updateBy=_user;}
+
+    void addKeyword(String _keyword) {
         keywords.add(_keyword);
     }
 
-    public void setKeywords(  ArrayList<String> _keywords){this.keywords=_keywords;}
+    void setKeywords(  ArrayList<String> _keywords){this.keywords=_keywords;}
 
     //Getters
-    public long getId() {
+    long getId() {
         return this.id;
     }
 
-    public String getProductNumber() {
+    String getProductNumber() {
         return this.productNumber;
     }
 
-    public String getDescription() {
+    String getDescription() {
         return this.description;
     }
 
-    public String getWebsiteUrl() {
+    String getWebsiteUrl() {
         return this.websiteUrl;
     }
 
-    public double getPrice(){
+    double getPrice(){
         return this.price;
     }
-    public String getPriceString(){
+
+    String getPriceString(){
         return "$"+String.valueOf(this.price);
     }
 
-    public User getUpdateBy(){
+    User getUpdateBy(){
         return this.updateBy;
     }
 
-    public Category getCategory(){return this.category;}
+    Category getCategory(){return this.category;}
 
-    public LocalDate getUpdateDate(){
+    LocalDate getUpdateDate(){
         return this.updateDate.toLocalDate();
     }
 
-    public ArrayList<String> getKkeywords(){
+    ArrayList<String> getKkeywords(){
         return this.keywords;
     }
 
