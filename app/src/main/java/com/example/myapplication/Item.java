71787be5770d44650faba734882d5ddf4ff8c536
Chris Heffley <heffcr01@pfw.edu>
2019-03-25 18:00:48 -0400

    Initial DB Code, Search and create activity fleshed out. Main activity wired up for testing DB

--- app/src/main/java/com/example/myapplication/Item.java
+++ app/src/main/java/com/example/myapplication/Item.java
@@ -7,38 +7,37 @@ import java.util.ArrayList;
 
 public class Item {
     protected long id;
-    private String productNumber;
-    private String description;
-    private String websiteUrl;
-    private double price;
-    private User updateBy;
-    private Category category;
-    private ArrayList<String> keywords;
-    private ZonedDateTime updateDate;
+    protected String productNumber;
+    protected String description;
+    protected String websiteUrl;
+    protected double price;
+    protected User updateBy;
+    protected Category category;
+    protected ArrayList<String> keywords;
+    protected ZonedDateTime updateDate;
 
+    Item() {
+    }
 
-    //private ShopcastDBHelper DBhelper;
-    Item (){}
     Item(String _productNumber,
          String _description,
          String _websiteUrl,
          double _price,
          User _user,
          Category _category,
-         ArrayList<String> _keywords){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy - HH:mm:ss Z");
-            this.id=0;
-            this.productNumber=_productNumber;
-            this.description = _description;
-            this.websiteUrl=_websiteUrl;
-            this.price = _price;
-            this.updateBy = _user;
-            this.category =_category;
-            this.keywords=_keywords;
-            this.updateDate= ZonedDateTime.now();
-
-
+         ArrayList<String> _keywords) {
+        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy - HH:mm:ss Z");
+        this.id = 0;
+        this.productNumber = _productNumber;
+        this.description = _description;
+        this.websiteUrl = _websiteUrl;
+        this.price = _price;
+        this.updateBy = _user;
+        this.category = _category;
+        this.keywords = _keywords;
+        this.updateDate = ZonedDateTime.now();
     }
+
     //Setters
     void setProductNumber(String _productNumber) {
         this.productNumber = _productNumber;
@@ -56,15 +55,21 @@ public class Item {
         this.price = _price;
     }
 
-    void setCategory(Category _category){this.category=_category; }
+    void setCategory(Category _category) {
+        this.category = _category;
+    }
 
-    void setUpdateUser(User _user){this.updateBy=_user;}
+    void setUpdateUser(User _user) {
+        this.updateBy = _user;
+    }
 
     void addKeyword(String _keyword) {
         keywords.add(_keyword);
     }
 
-    void setKeywords(  ArrayList<String> _keywords){this.keywords=_keywords;}
+    void setKeywords(ArrayList<String> _keywords) {
+        this.keywords = _keywords;
+    }
 
     //Getters
     long getId() {
@@ -83,27 +88,27 @@ public class Item {
         return this.websiteUrl;
     }
 
-    double getPrice(){
+    double getPrice() {
         return this.price;
     }
 
-    String getPriceString(){
-        return "$"+String.valueOf(this.price);
+    String getPriceString() {
+        return "$" + String.valueOf(this.price);
     }
 
-    User getUpdateBy(){
+    User getUpdateBy() {
         return this.updateBy;
     }
 
-    Category getCategory(){return this.category;}
+    Category getCategory() {
+        return this.category;
+    }
 
-    LocalDate getUpdateDate(){
+    LocalDate getUpdateDate() {
         return this.updateDate.toLocalDate();
     }
 
-    ArrayList<String> getKkeywords(){
+    ArrayList<String> getKkeywords() {
         return this.keywords;
     }
-
-
 }
