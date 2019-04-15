3ed0085ea311927bdfab8cce2a72003e16c9d846
Hieu <luuht01@pfw.edu>
2019-04-15 16:38:51 -0400

    search + user + register

---
+++ app/src/main/java/com/example/myapplication/UserDBContract.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.provider.BaseColumns;
+
+public class UserDBContract
+{
+    public static class UserEntry implements BaseColumns {
+        public static final String TableName = "Users";
+        public static final String ColUserId = "UserID";
+        public static final String ColUsername = "Username";
+        public static final String ColPassword = "Password";
+    }
+
+    public static final String CreateTable =
+            "Create Table " + UserEntry.TableName + " (" +
+                    UserEntry.ColUserId + " integer primary key autoincrement, " +
+                    UserEntry.ColUsername + " text, " +
+                    UserEntry.ColPassword + " text); ";
+
+    public static final String DropTable = "drop table if exists " + UserEntry.TableName;
+}
