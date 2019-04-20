9596ce1ed02df9492dacc48ee9ccf1dca7ce8725
Hieu <luuht01@pfw.edu>
2019-04-20 13:07:15 -0400

    register database work

---
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -0,0 +1,64 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Register extends AppCompatActivity {
+    Button registerBTN, cancelBTN;
+    EditText UsernameTXT, PasswordTXT;
+    UserDB userDB;
+    CategoryDB categoryDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        this.userDB = new UserDB(this);
+        this.categoryDb = new CategoryDB(this);
+        this.registerBTN = findViewById(R.id.registerBTN);
+        this.cancelBTN = findViewById(R.id.cancelBTN);
+        this.UsernameTXT = findViewById(R.id.usernameTXT);
+        this.PasswordTXT = findViewById(R.id.passwordTXT);
+
+        this.categoryDb = new CategoryDB(this);
+        ArrayList<Category> categoryArray = categoryDb.getCategories();
+
+        this.cancelBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        this.registerBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User _user = new User(UsernameTXT.getText().toString(),
+                    PasswordTXT.getText().toString());
+                long partKey = userDB.add(_user);
+                if (partKey > 0) {
+                    Toast.makeText(getApplicationContext(), "New user added", Toast.LENGTH_SHORT).show();
+                    Intent activityIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(activityIntent);
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Failed to add user", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+}
